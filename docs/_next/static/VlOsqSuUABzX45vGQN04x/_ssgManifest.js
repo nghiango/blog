c10a0fa3cef6e246aaa38e5a0980ce38821675ea
Nghia Ngo <nghia.ngo@ubitec.com>
2022-03-20 10:21:15 +0700

    update static page

--- docs/_next/static/VlOsqSuUABzX45vGQN04x/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fsecurity\u002F[slug]","\u002Fother\u002F[slug]","\u002Fguide\u002F[slug]","\u002Fintegration\u002F[slug]","\u002Fautomation\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
