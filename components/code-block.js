6a2c8a0bc90b7630030f984cb4372ac1d5aeff5c
Nghia Ngo <nghia.ngo@mesoneer.io>
2022-06-26 01:54:35 +0700

    Add button copy into code block

--- components/code-block.js
+++ components/code-block.js
@@ -1,11 +1,14 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { darcula } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { darcula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { FaRegCopy } from "react-icons/fa"
 export function CodeBlock(props) {
   const match = /language-(\w+)/.exec(props.className || "");
   return !props.inline && match ? (
-    <SyntaxHighlighter language={match[1]} PreTag="div" style={darcula}>
-      {props.value}
-    </SyntaxHighlighter>
+    <div className="code-block__block">
+      <CopyToClipboard className="code-block__copy-func" text={props.value}><FaRegCopy/></CopyToClipboard>
+      <SyntaxHighlighter language={match[1]} style={darcula}>{props.value}</SyntaxHighlighter>
+    </div>
   ) : (
     <code className={className} {...props}>
       {props.value}
